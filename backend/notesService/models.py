72a4fdfe4ce269b9ec4e4bd8d59e0138fab94736
Mohammed Khair <55262573+Mohammed12Khair@users.noreply.github.com>
2023-09-02 15:47:14 +0400

    create notes model
    - create note serialize
    - add dummy data for testing

--- backend/notesService/models.py
+++ backend/notesService/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Notes(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField(max_length=1000)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
