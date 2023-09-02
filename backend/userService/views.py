a253a8d4c98d183257979d71edde1353846d0b55
Mohammed Khair <55262573+Mohammed12Khair@users.noreply.github.com>
2023-09-02 14:45:05 +0400

    Set userService
    - create serializer for User model
    - add urls to userService and on project urls
    - import needed on userService.views
    - migrate
    - run one test to confirm no errors

--- backend/userService/views.py
+++ backend/userService/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+##
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
+from .serializer import UserSerializer
+from rest_framework.authentication import SessionAuthentication
+from knox.auth import AuthToken, TokenAuthentication
 
-# Create your views here.
+
+@api_view(['GET'])
+def Test_view(request):
+    return Response({"data": "server"})
